d89ee979e895ade7e7c106b4f082899e31fdaea7
sunyc <sun@mys4s.cn>
2017-10-14 16:29:29 +0800

    init

---
+++ Podfile
@@ -0,0 +1,12 @@
+platform :ios, '9.0'
+use_frameworks!
+target 'weibo' do
+pod 'Alamofire'
+pod 'SwiftyJSON'
+pod 'HandyJSON'
+pod 'CRRefresh'
+pod 'AlamofireImage', '~> 3.3'
+pod 'SwiftDate', '~> 4.3.0'
+pod 'SnapKit', '~> 3.0.2'
+pod 'Kingfisher', '~> 3.13.1'
+end
---
+++ weibo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  weibo
+//
+//  Created by sunyuchun on 2017/10/14.
+//  Copyright © 2017年 sunyc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
