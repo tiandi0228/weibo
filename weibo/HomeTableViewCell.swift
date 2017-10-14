3a90c164eec9a4ea57e32b1d90e82dd0351998e5
sunyc <sun@mys4s.cn>
2017-10-14 16:51:42 +0800

    init

---
+++ weibo/HomeTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.swift
+//  weibo
+//
+//  Created by sunyuchun on 2017/10/14.
+//  Copyright © 2017年 sunyc. All rights reserved.
+//
+
+import UIKit
+
+class HomeTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
